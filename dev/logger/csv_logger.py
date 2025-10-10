733cfac54460a8d0dc56ebd37081c6335f9208d7
Michael Ryan <michael_ryan_2000@yahoo.com>
2025-10-10 02:10:21 -0700

    Added Calendar Observer

---
+++ dev/logger/csv_logger.py
@@ -0,0 +1,30 @@
+import asyncio
+from gum import gum
+from gum.observers import Screen
+from cal import Calendar
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user_name = "Michael Ryan"
+model = "gpt-4o-mini-2024-07-18"
+max_batch_size = 15
+
+async def log_observations(observer, update):
+    print("=== Logging observations ===")
+    print(observer, update)
+    print("=== === === === === === ===")
+
+async def main():
+    async with gum(
+        user_name, 
+        model, 
+        # Screen(model),
+        Calendar(),
+        max_batch_size=max_batch_size
+    ) as gum_instance:
+        gum_instance.register_update_handler(log_observations)
+        await asyncio.Future()  # run forever (Ctrl-C to stop)
+
+if __name__ == "__main__":
+    asyncio.run(main())
